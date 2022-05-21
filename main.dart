d073520cc026dfd791eeae4b25ebbdb232bec2f2
Mawada alrashed <mawadaalrashed98@gmail.com>
2022-05-21 14:01:47 -0700

    the first commit

---
+++ main.dart
@@ -0,0 +1,16 @@
+
+void main(List<String> arguments){
+
+ //the firt object
+
+Mobile SamsongGalaxyA20 =  Mobile();
+SamsongGalaxyA20.getActualPrice(20);
+SamsongGalaxyA20.printDetails(Brand="galaxy", Model="A20", MRP=300$);
+
+
+//the second object
+
+Mobile SamsongGalaxyGrandprime = Mobile();
+SamsongGalaxyGrandprime.getActualPrice(50);
+SamsongGalaxyGrandprime.printDetails(Brand="galaxy", Model="prime", MRP=240$);
+}
